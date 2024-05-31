fe4cade8bea2c25ff8fe055737f00293b98370fd
alejandro <morenopilaralejandro@gmail.com>
2024-05-31 19:19:49 +0200

    index buttons

--- src/main/java/com/choujigen/ogre/controller/WebController.java
+++ src/main/java/com/choujigen/ogre/controller/WebController.java
@@ -1,5 +1,8 @@
 package com.choujigen.ogre.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ public class WebController {
 
 	@RequestMapping(value = "/")
 	public String index(Model model) {
+		Locale locale = LocaleContextHolder.getLocale();
+		model.addAttribute("currentLang", locale.getLanguage());
 		return "/index";
 	}
 
